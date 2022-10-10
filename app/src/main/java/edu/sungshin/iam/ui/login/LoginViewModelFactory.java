1f85504c5461324c89c8419cc591e63cf7665135
siyeon yoo <syne77777@gmail.com>
2022-10-10 14:26:41 +0900

    login

---
+++ app/src/main/java/edu/sungshin/iam/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package edu.sungshin.iam.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import edu.sungshin.iam.data.LoginDataSource;
+import edu.sungshin.iam.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
