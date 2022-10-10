73ca895d3851d18f59272405386541a996f4b4f7
yugangee <113881661+yugangee@users.noreply.github.com>
2022-10-10 17:12:34 +0900

    112 119

--- app/src/main/java/edu/sungshin/iam/Safety.java
+++ app/src/main/java/edu/sungshin/iam/Safety.java
@@ -2,13 +2,38 @@ package edu.sungshin.iam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Safety extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mlntent = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:/112"));
+                startActivity(mlntent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mlntent = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:/119"));
+                startActivity(mlntent);
+            }
+        });
     }
 }
